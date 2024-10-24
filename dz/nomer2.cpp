061c1719c23fa2145b853341fa510a2facbc430b
igor05max <94913244+igor05max@users.noreply.github.com>
2024-10-24 23:37:52 +0300

    Update nomer2.cpp

--- dz/nomer2.cpp
+++ dz/nomer2.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-/*
- Задание 2
-*/
+
+
 int main() {
+    /*
+    * 0 ->
+    * 1 V
+    * 2 <-
+    * 3 ^
+    */
 
     setlocale(LC_ALL, "Russian");
     
-    // a)
-    cout << "a)" << endl;
-    double territory = 100, profit = 20;
-    for (int i = 2; i <= 8; ++i) {
-        profit *= 1.02;
-        cout << "урожайность за " << i << " год = " << profit << endl;
+    vector<vector<int>> mass(5, { 0, 0, 0, 0, 0 });
+
+    bool run = true;
+
+    int i = 0, j = -1;
+    vector<vector<int>> orint = {
+        {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+    };
+    
+    int orint_index = 0;
+    int ai = orint[orint_index][0], aj = orint[orint_index][1];
+    int hod = 0;
+    
+    while (true) {
+        
+        if (0 <= i + ai && i + ai < 5 && 0 <= j + aj && j + aj < 5 && mass[i + ai][j + aj] == 0) {
+            hod++;
+            mass[i + ai][j + aj] = hod;
+            run = true;
+            i += ai;
+            j += aj;
+
+        }
+        else if (!run) {
+            break;
+        }
+        else {
+            orint_index = (orint_index + 1) % 4;
+            ai = orint[orint_index][0];
+            aj = orint[orint_index][1];
+            run = false;
+        }
     }
 
-    // б)
-    cout << "б)" << endl;
-    for (int i = 2; i <= 7; ++i) {
-        territory *= 1.05;
-        if (i >= 4)
-            cout << "площадь участка за " << i << " год = " << territory << endl;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cout << mass[i][j] << "\t";
+        }
+        cout << endl;
     }
-
-    // в) 
-    cout << "в)" << endl;
-    territory = 100, profit = 20;
-    double summ = profit * territory;
-    for (int i = 2; i <= 6; ++i) {
-        profit *= 1.02;
-        territory *= 1.05;
-        summ += profit * territory;
-    }
-    cout << "урожай за первые шесть лет: " << summ << endl;
 
     return 0;
 }
