5a01d7ba10208e8050459d05c37836e08a93b972
igor05max <94913244+igor05max@users.noreply.github.com>
2024-10-24 23:37:01 +0300

    Update numer1.cpp

--- dz/numer1.cpp
+++ dz/numer1.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
-/*
- Задание 1
-*/
+
 int main() {
 
     setlocale(LC_ALL, "Russian");
-    // a)
-    cout << "a)" << endl;
-    double summ = 1000;
-    for (int i = 1; i <= 10; ++i) {
-        double res = summ * .02;
-        cout << "прирост за " << i << " месяц " << res << endl;
-        summ += res;
-    }
-
-    // б)
-    cout << "б)" << endl;
-    summ = 1000;
-    for (int i = 1; i <= 10; ++i) {
-        summ *= 1.02;
-        cout << "сумма вклада через " << i << " мес. " << summ << endl;
-    }
     
+    bool ts = true;
+    int contl = 0;
 
+    for (int i = 1; i <= 25; ++i) {
+        if (ts)
+            cout << i << "\t";
+        else {
+            cout << i + contl << "\t";
+            contl -= 2;
+        }
+        if (i % 5 == 0) {
+            cout << endl;
+            ts = !ts;
+            contl = 4;
+        }
+    }
     return 0;
 }
