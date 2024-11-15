435835d0838ac2c990f5851508071f1995435c15
igor05max <94913244+igor05max@users.noreply.github.com>
2024-11-15 03:07:24 +0300

    Update task1.cpp

--- kl/task1.cpp
+++ kl/task1.cpp
@@ -1,121 +1,98 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
-/*
-Дан массив из четного числа
-элементов. Поменять местами его
-половины;*/
-void task() {
-	int n;
-	cin >> n;
-	vector<int> mass(n);
+
+void new_list(vector<vector<int>> &mass, int n, int m) {
 	for (int i = 0; i < n; ++i) {
-		cin >> mass[i];
-	}
-
-	for (int i = 0; i < n / 2; ++i)
-		swap(mass[i], mass[i + n / 2]);
-
-	for (int i = 0; i < n; ++i)
-		cout << mass[i] << " ";
-
-}
-
-
-/*
-Дан массив целых чисел. Найти
-номер первого нечетного элемента.
-Если нечетных элементов в массиве
-нет, то должно быть выведено
-соответствующее сообщение;*/
-void task2() {
-	int n;
-	cin >> n;
-	vector<int> mass(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> mass[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		if (mass[i] % 2 == 1) {
-			cout << i + 1;
-			return;
+		for (int j = 0; j < m; ++j) {
+			mass[i][j] = rand() % 10 + 1;
 		}
 	}
-	cout << "нет такого числа";
+
 }
 
 
-/*
-Даны два массива одного размера. Получить
-третий массив, каждый четный элемент
-которого равен произведению элементов с
-тем же номером в заданных массивах, а
-каждый нечетный – сумме элементов с тем
-же номером в заданных массивах*/
+void print(vector<vector<int>> mass) {
+	for (int i = 0; i < mass.size(); ++i) {
+		for (int j = 0; j < mass[0].size(); ++j) {
+			cout << mass[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+
+void task() {
+	vector<vector<int>> mass(12, vector<int>(10));
+	new_list(mass, 12, 10);
+	for (int i = 1; i <= 12; ++i) {
+		for (int j = i * 10, jj = 0; j >= i * 10 - 9; --j, ++jj) {
+			mass[i - 1][jj] = j;
+		}	
+	}
+	print(mass);
+}
+
+
+void task2() {
+	vector<vector<int>> mass(11, vector<int>(4));
+	new_list(mass, 11, 4);
+	print(mass);
+
+	int res = 0;
+	for (int i = 0; i < 4; ++i)
+		res += mass[4][i];
+	cout << res << endl;
+}
+
+
 void task3() {
-	int n;
-	cin >> n;
-	vector<int> mass(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> mass[i];
-	}
+	vector<vector<int>> mass(10, vector<int>(10));
+	new_list(mass, 10, 10);
+	print(mass);
 
-	vector<int> mass2(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> mass2[i];
+	int i_best = 0;
+	for (int i = 1; i < 10; ++i) {
+		if (mass[i][2] >= mass[i_best][2])
+			i_best = i;
 	}
-
-	vector<int> res(n);
-	for (int i = 0; i < n; ++i) {
-		if (i % 2 == 0)
-			res[i] = mass[i] * mass2[i];
-		else
-			res[i] = mass[i] + mass2[i];
-	}
-	for (int i = 0; i < n; ++i) {
-		cout << res[i] << " ";
-	}
+	cout << i_best + 1 << endl;
 }
 
-
-/*
-Дан массив вещественных
-чисел. Выяснить, верно ли,
-что минимальный элемент
-меньше максимального
-более чем в два раза*/
 
 void task4() {
-	int n;
-	cin >> n;
-	vector<double> mass(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> mass[i];
+	vector<vector<int>> mass(10, vector<int>(10));
+	new_list(mass, 10, 10);
+	print(mass);
+	
+	int j_best = 10;
+	for (int i = 0; i < 10; ++i) {
+		int j_min = 0;
+		for (int j = 1; j < 10; ++j) {
+			if (mass[i][j] < mass[i][j_min])
+				j_min = j;
+		}
+		j_best = min(j_min, j_best);
 	}
-	double mx = DBL_MIN, mn = DBL_MAX;
-	for (double x : mass) {
-		mx = max(x, mx);
-		mn = min(x, mn);
-	}
-	if (mn * 2 < mx) {
-		cout << "yes";
-	}
-	else {
-		cout << "no";
-	}
+	cout << j_best + 1 << endl;
+
 }
 
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	srand(time(0));
 
-	/*task1();
-	task2();
-	task3();*/
+	//task();
+	//task2();
+	//task3();
 	task4();
+
+
 
 	return 0;
 
