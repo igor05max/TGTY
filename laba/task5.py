140f5f4b9654617efb306fb22f1296396108b670
igor05max <94913244+igor05max@users.noreply.github.com>
2025-02-27 00:06:50 +0300

    Update task5.py

--- laba/task5.py
+++ laba/task5.py
@@ -1,9 +1,8 @@
-def SumList(mass: list[list[float]]) -> int:
-    res = 0
-    for line in mass:
-        for i in line:
-            res += i * i
-    return res
-
+def SumList(mass: list[list[float]]) -> float:
+    res = 0 
+    for line in mass: 
+        for i in line: 
+            res += i * i 
+    return res 
 
 print(SumList([[1, 2], [3, 4]]))
