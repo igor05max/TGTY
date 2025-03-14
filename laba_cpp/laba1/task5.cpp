57e499fa14fdb38e56d22a592a7f1dd04f989432
igor05max <94913244+igor05max@users.noreply.github.com>
2025-03-14 14:00:23 +0300

    Create task5.cpp

---
+++ laba_cpp/laba1/task5.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    char ifname[20];
+    char ofname[20];
+
+    cout << "Введите имя исходного файла: ";
+    cin >> ifname;
+
+
+    FILE* inputFile = fopen(ifname, "r");
+    if (!inputFile) {
+        cerr << "Ошибка открытия файла. Возможно файл отсутствует: " << ifname << endl;
+        return 1;
+    }
+
+
+    char str[100];
+    int strNumber = 0;
+
+    while (fgets(str, sizeof(str), inputFile)) {
+        strNumber++;
+        if (strNumber % 2 == 0) {
+            if (str[strlen(str) - 1] == '\n')
+                str[strlen(str) - 1] = ' ';
+            cout << str;
+        }
+    }
+
+    fclose(inputFile);
+
+    
+
+    return 0;
+}
